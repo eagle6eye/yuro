5459437e267e26924b4a140bd4c393839eec41cb
eagle6eye <a_egal3@hotmail.com>
2018-11-15 21:23:16 -0500

    website update

---
+++ build/precache-manifest.c165744ac9436d2b09cd15862ebbd6e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6755414fd00f91aab361317ccae9778d",
+    "url": "/eagle6eye/yuro/static/media/logo.6755414f.png"
+  },
+  {
+    "revision": "5c236b584cc848ddee8e",
+    "url": "/eagle6eye/yuro/static/js/runtime~main.5c236b58.js"
+  },
+  {
+    "revision": "a3ccedf10f189127ed06",
+    "url": "/eagle6eye/yuro/static/js/main.a3ccedf1.chunk.js"
+  },
+  {
+    "revision": "6a9c29ae630188744494",
+    "url": "/eagle6eye/yuro/static/js/1.6a9c29ae.chunk.js"
+  },
+  {
+    "revision": "a3ccedf10f189127ed06",
+    "url": "/eagle6eye/yuro/static/css/main.cdce07be.chunk.css"
+  },
+  {
+    "revision": "098f6ff122bd8558e8b4083faab9b45f",
+    "url": "/eagle6eye/yuro/index.html"
+  }
+];
